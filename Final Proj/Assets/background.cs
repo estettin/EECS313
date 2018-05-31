2c3ce8ab9bb8c530b7835142248f8ebddcf50011
Jake Reifer <JakeReifer@dhcp-10-105-228-192.wireless.northwestern.private>
2018-05-31 15:26:41 -0500

    Finished background and key

---
+++ Final Proj/Assets/background.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class background : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<MeshRenderer>().material.color = Color.blue;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Final Proj/Assets/keybackground.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class keybackground : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<MeshRenderer>().material.color = Color.green;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
